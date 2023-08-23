90e15c381e631aaa01bf74950f0d33cb6cfa9ecd
Roman Metelov <rm0734861369@gmail.com>
2023-08-23 17:13:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/youtubeclone/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.youtubeclone
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.FrameLayout
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.youtubeclone.databinding.ActivityMainBinding
+import com.example.youtubeclone.fragment.ExploreFragment
+import com.example.youtubeclone.fragment.HomeFragment
+import com.example.youtubeclone.fragment.LibraryFragment
+import com.example.youtubeclone.fragment.SubscriptionsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var frameLayout: FrameLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        bottomNavigationView = binding.bottomNavigation
+        frameLayout = binding.frameLayout
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    val homeFragment = HomeFragment()
+                    selectedFragment(homeFragment)
+                    true
+                }
+                R.id.explore -> {
+                    val exploreFragment = ExploreFragment()
+                    selectedFragment(exploreFragment)
+                    true
+                }
+                R.id.publish -> {
+                    Toast.makeText(this, "Add a video", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.subscriptions -> {
+                    val subscriptionsFragment = SubscriptionsFragment()
+                    selectedFragment(subscriptionsFragment)
+                    true
+                }
+                R.id.library -> {
+                    val libraryFragment = LibraryFragment()
+                    selectedFragment(libraryFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        bottomNavigationView.selectedItemId = R.id.home
+
+    }
+
+    private fun selectedFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+}
